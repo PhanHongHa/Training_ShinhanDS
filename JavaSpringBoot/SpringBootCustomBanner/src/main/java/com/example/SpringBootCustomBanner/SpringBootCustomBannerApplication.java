ed31fc7ac96e00f25d43f6e214f859e375590d13
ui2 <phanhongha96@gmail.com>
2023-01-04 18:54:55 +0700

    Week4-1 Spring Boot Custom Banner

---
+++ JavaSpringBoot/SpringBootCustomBanner/src/main/java/com/example/SpringBootCustomBanner/SpringBootCustomBannerApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootCustomBanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCustomBannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCustomBannerApplication.class, args);
+	}
+
+}
---
+++ JavaSpringBoot/SpringBootCustomBanner/src/test/java/com/example/SpringBootCustomBanner/SpringBootCustomBannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootCustomBanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCustomBannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
