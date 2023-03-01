55bcc2f2fc6d618105e5d6ed1c307f52ee5797b4
ui2 <phanhongha96@gmail.com>
2023-03-01 23:04:59 +0700

    demo Redis || demo WebSocket

---
+++ JavaSpringBoot/demoRedis/src/main/java/com/example/demoRedis/DemoRedisApplication.java
@@ -0,0 +1,38 @@
+package com.example.demoRedis;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootApplication
+public class DemoRedisApplication {
+
+	@Autowired
+	private RedisTemplate template;
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRedisApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(){
+		return args ->{
+			listExample();
+		};
+	}
+
+	public void listExample(){
+		List<String> list = new ArrayList<>();
+		list.add("hello");
+		list.add("redis");
+		template.opsForList().rightPushAll("likelion_list", list);
+		System.out.println("Size of key likelion:"+template.opsForList().size("likelion_list"));
+	}
+
+
+}
